25ccc217846340440210205550de81b4786e6035
Erik Ekblad <oakl33f123@gmail.com>
2025-11-04 15:59:13 +0100

    changed function for owned u8's for more safaty needs a little redo of partial reads where it shoudl be able to read the command as well if the header is read but not the command

--- src/protocol/parser.rs
+++ src/protocol/parser.rs
@@ -1,24 +1,26 @@
 use crate::error::ProtocolError;
 
+#[derive(PartialEq, Eq, Debug)]
 pub enum ParseState {
     Empty,
     Partial,
     Complete,
 }
 
-pub struct CommandParseState<'a> {
-    command_name: Option<&'a [u8]>,
-    args: Vec<&'a [u8]>,
+#[derive(PartialEq, Eq, Debug)]
+pub struct CommandParseState {
+    command_name: Option<Vec<u8>>, // change this to an enum later
+    args: Vec<Vec<u8>>,
     expected_args: usize,
     current_arg: usize,
     state: ParseState,
 }
 
-impl<'a> CommandParseState<'a> {
-    pub fn new() -> CommandParseState<'a> {
+impl CommandParseState {
+    pub fn new() -> CommandParseState {
         CommandParseState {
             command_name: None,
-            args: Vec::<&'a [u8]>::new(),
+            args: Vec::<Vec<u8>>::new(),
             expected_args: 0,
             current_arg: 0,
             state: ParseState::Empty,
@@ -34,13 +36,11 @@ impl<'a> CommandParseState<'a> {
     }
 }
 
-fn parse_command<'a>(
-    buf: &'a [u8],
+fn parse_command(
+    buf: &[u8],
     pos: &mut usize,
-    command_parse_state: &'a mut CommandParseState<'a>,
+    command_parse_state: &mut CommandParseState,
 ) -> Result<(), ProtocolError> {
-    command_parse_state.state = ParseState::Partial;
-
     let amount_strings = parse_array_header(buf, pos)?;
 
     let command = parse_bulk_string(buf, pos)?;
@@ -50,12 +50,32 @@ fn parse_command<'a>(
         return Err(ProtocolError::WrongNumberOfArguments);
     }
 
-    command_parse_state.command_name = Some(command);
+    command_parse_state.command_name = Some(command.to_vec());
     command_parse_state.expected_args = amount_arguments;
+
+    command_parse_state.state = ParseState::Partial;
 
     for _ in 0..amount_strings - 1 {
         let argument = parse_bulk_string(buf, pos)?;
-        command_parse_state.args.push(argument);
+        command_parse_state.current_arg += 1;
+        command_parse_state.args.push(argument.to_vec());
+    }
+
+    command_parse_state.state = ParseState::Complete;
+    Ok(())
+}
+
+fn parse_partial_command(
+    buf: &[u8],
+    pos: &mut usize,
+    command_parse_state: &mut CommandParseState,
+) -> Result<(), ProtocolError> {
+    let amount_arguments = command_parse_state.expected_args - command_parse_state.current_arg;
+
+    for _ in 0..amount_arguments {
+        let argument = parse_bulk_string(buf, pos)?;
+        command_parse_state.current_arg += 1;
+        command_parse_state.args.push(argument.to_vec());
     }
 
     command_parse_state.state = ParseState::Complete;
@@ -338,6 +358,135 @@ mod tests {
             }
 
             i += 1;
+        }
+    }
+
+    #[test]
+    fn test_parse_command() {
+        struct TestData {
+            buffer: &'static [u8],
+            expected_position: usize,
+            expected_state: CommandParseState,
+        }
+
+        let tests = vec![
+            TestData {
+                buffer: b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n",
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"GET".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 1,
+                    current_arg: 1,
+                    state: ParseState::Complete,
+                },
+            },
+            TestData {
+                buffer: b"*2\r\n$3\r\nDEL\r\n$5\r\nhello\r\n",
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"DEL".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 1,
+                    current_arg: 1,
+                    state: ParseState::Complete,
+                },
+            },
+            TestData {
+                buffer: b"*3\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld\r\n",
+                expected_position: 35,
+                expected_state: CommandParseState {
+                    command_name: Some(b"SET".to_vec()),
+                    args: vec![b"hello".to_vec(), b"world".to_vec()],
+                    expected_args: 2,
+                    current_arg: 2,
+                    state: ParseState::Complete,
+                },
+            },
+            TestData {
+                buffer: b"*3\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld",
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"SET".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 2,
+                    current_arg: 1,
+                    state: ParseState::Partial,
+                },
+            },
+        ];
+
+        for test in tests {
+            let mut parse_state = CommandParseState::new();
+            let mut position = 0;
+
+            let _ = parse_command(test.buffer, &mut position, &mut parse_state);
+
+            assert_eq!(test.expected_position, position);
+            assert_eq!(test.expected_state, parse_state);
+        }
+    }
+
+    #[test]
+    fn test_parse_partial_command() {
+        struct TestData {
+            buffer: Vec<&'static [u8]>,
+            expected_position: usize,
+            expected_state: CommandParseState,
+        }
+
+        let tests = vec![
+            TestData {
+                buffer: vec![b"*2\r\n$3\r\nGET\r\n", b"$5\r\nhello\r\n"],
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"GET".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 1,
+                    current_arg: 1,
+                    state: ParseState::Complete,
+                },
+            },
+            TestData {
+                buffer: vec![b"*2\r\n$3\r\nDEL\r\n", b"$5\r\n", b"hello\r\n"],
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"DEL".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 1,
+                    current_arg: 1,
+                    state: ParseState::Complete,
+                },
+            },
+            TestData {
+                buffer: vec![b"*2\r\n$3\r\nDEL\r\n", b"$5", b"\r", b"\n", b"hello\r\n"],
+                expected_position: 24,
+                expected_state: CommandParseState {
+                    command_name: Some(b"DEL".to_vec()),
+                    args: vec![b"hello".to_vec()],
+                    expected_args: 1,
+                    current_arg: 1,
+                    state: ParseState::Complete,
+                },
+            },
+        ];
+
+        for test in tests {
+            let mut parse_state = CommandParseState::new();
+            let mut position = 0;
+
+            let mut test_buffer = Vec::<u8>::new();
+            test_buffer.extend_from_slice(test.buffer[0]);
+            let _ = parse_command(&test_buffer, &mut position, &mut parse_state);
+
+            for i in 1..test.buffer.len() {
+                test_buffer.extend_from_slice(test.buffer[i]);
+
+                let _ = parse_partial_command(&test_buffer, &mut position, &mut parse_state);
+            }
+
+            assert_eq!(test.expected_position, position);
+            assert_eq!(test.expected_state, parse_state);
         }
     }
 }
