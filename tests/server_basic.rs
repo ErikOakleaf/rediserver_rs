af835c75aa115a13b09cbf5a101579e2c3712c44
Erik Ekblad <oakl33f123@gmail.com>
2025-11-12 16:18:31 +0100

    added some integration tests

--- tests/server_basic.rs
+++ tests/server_basic.rs
@@ -3,25 +3,94 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
-fn main() -> std::io::Result<()> {
+#[test]
+fn test_basic_server_commands() -> std::io::Result<()> {
     // spawn server in another thread
+
     thread::spawn(|| {
-        redis::server::main().unwrap();
+        let mut server = redis::server::Server::new(0, 1234).unwrap();
+        server.run().unwrap();
     });
 
     // give server a moment to start
     thread::sleep(Duration::from_millis(200));
 
+    // connect socket to server
     let mut stream = TcpStream::connect("127.0.0.1:1234")?;
 
-    // send a command
-    let cmd = b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
-    stream.write_all(cmd)?; // write all bytes
+    struct TestData {
+        command: &'static [u8],
+        expected: &'static [u8],
+    }
 
-    // let's say we expect a reply of exactly 5 bytes: "+OK\r\n"
-    let mut buf = [0u8; 5];
-    stream.read_exact(&mut buf)?; // blocks until all 5 bytes are received
+    let tests = vec![
+        //basic functionality
+        TestData {
+            command: b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+            expected: b"+OK\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
+            expected: b"$3\r\nbar\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nDEL\r\n$3\r\nfoo\r\n",
+            expected: b":1\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
+            expected: b"$-1\r\n",
+        },
+        // test overwriting sets
+        TestData {
+            command: b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+            expected: b"+OK\r\n",
+        },
+        TestData {
+            command: b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbaz\r\n",
+            expected: b"+OK\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
+            expected: b"$3\r\nbaz\r\n",
+        },
+        // multiple deletes
+        TestData {
+            command: b"*3\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld\r\n",
+            expected: b"+OK\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n",
+            expected: b"$5\r\nworld\r\n",
+        },
+        TestData {
+            command: b"*3\r\n$3\r\nDEL\r\n$3\r\nfoo\r\n$5\r\nhello\r\n",
+            expected: b":2\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
+            expected: b"$-1\r\n",
+        },
+        TestData {
+            command: b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n",
+            expected: b"$-1\r\n",
+        },
+    ];
 
-    assert_eq!(&buf, b"+OK\r\n");
+    for test in tests {
+        stream.write_all(test.command)?;
+
+        let mut buf = vec![0u8; test.expected.len()];
+        stream.read_exact(&mut buf)?;
+
+        assert_eq!(
+            buf.as_slice(),
+            test.expected,
+            "expected {:?}\ngot: {:?}",
+            buf.as_slice(),
+            test.expected
+        );
+    }
+
     Ok(())
 }
